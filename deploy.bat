159cf4f0bd35cd9dfbc34e6b8418c6221834ca79
bss08380 <brandon.smithers9@gmail.com>
2015-08-26 07:48:39 -0400

    Pre-Reorganization Delete
    Deleting project files prior to reorganization under a different
    directory structure.

--- deploy.bat
+++
@@ -1,4 +0,0 @@
-"C:\Program Files (x86)\PuTTY\pscp.exe" .\*.htm* ubuntu@brandonsmithers.com:/var/www/html/
-"C:\Program Files (x86)\PuTTY\pscp.exe" .\*.css ubuntu@brandonsmithers.com:/var/www/html/
-"C:\Program Files (x86)\PuTTY\pscp.exe" .\*.pdf ubuntu@brandonsmithers.com:/var/www/html/
-"C:\Program Files (x86)\PuTTY\pscp.exe" .\*.jpg ubuntu@brandonsmithers.com:/var/www/html/
