90848d31f69b174cd7e004318b0d8657da99c596
bss08380 <brandon.smithers9@gmail.com>
2015-08-26 07:49:36 -0400

    Upload Files
    Uploading of project files, rearranged in a new directory structure

---
+++ deploy.bat
@@ -0,0 +1,7 @@
+"C:\Program Files (x86)\PuTTY\pscp.exe" .\*.html ubuntu@brandonsmithers.com:/var/www/html/
+"C:\Program Files (x86)\PuTTY\pscp.exe" .\html\* ubuntu@brandonsmithers.com:/var/www/html/html/
+"C:\Program Files (x86)\PuTTY\pscp.exe" .\css\* ubuntu@brandonsmithers.com:/var/www/html/css/
+"C:\Program Files (x86)\PuTTY\pscp.exe" .\js\* ubuntu@brandonsmithers.com:/var/www/html/js/
+"C:\Program Files (x86)\PuTTY\pscp.exe" .\img\* ubuntu@brandonsmithers.com:/var/www/html/img/
+"C:\Program Files (x86)\PuTTY\pscp.exe" .\res\* ubuntu@brandonsmithers.com:/var/www/html/res/
+start "" http://brandonsmithers.com
